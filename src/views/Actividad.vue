46dd09730a8b34d4edb1d975c8c3b9ba8329d5bd
maronita <maronita@users.noreply.github.com>
2022-09-08 10:38:32 -0500

    AD-MATERIAL

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -10,7 +10,7 @@
             img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Actividad didáctica 1 - Afirmaciones sobre la actividad agrícola
+          h2  Afirmaciones sobre la actividad agrícola
         p.mb-4 Evaluar los conocimientos del aprendiz a través de preguntas de falso y verdadero de acuerdo a la información suministrada en los contenidos disponibles.
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
@@ -20,23 +20,7 @@
               a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
-    .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
-      .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
-        .tarjeta.h-100.d-flex.align-items-center.p-4
-          figure
-            img(src="@/assets/template/arrastrar.svg", alt="Texto que describa la imagen")
-      .col-12.col-md-8.col-lg-9
-        .titulo-segundo
-          h2 Actividad didáctica 2 - Términos y definiciones en producción agrícola
-        p.mb-4 Relacionar los conceptos propuestos de producción agrícola con su respectiva correspondencia, de acuerdo con los conocimientos adquiridos en el desarrollo del componente formativo.
-        .tarjeta.actividad.p-3
-          .row.justify-content-around.align-items-center            
-            .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Relacionamiento de columnas
-            .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
-                span Realizar
-                i.fas.fa-puzzle-piece
+    
 
    
 
