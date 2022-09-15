f66b43f401a6c0aca3499b2fa4bfe9af0c0caf14
maronita <maronita@users.noreply.github.com>
2022-09-15 08:39:35 -0500

    AD2-MATERIAL

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -10,7 +10,7 @@
             img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2  Afirmaciones sobre la actividad agrícola
+          h2  Actividad didactica 1 - Afirmaciones sobre la actividad agrícola
         p.mb-4 Evaluar los conocimientos del aprendiz a través de preguntas de falso y verdadero de acuerdo a la información suministrada en los contenidos disponibles.
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
@@ -20,8 +20,26 @@
               a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
-    
 
+    .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
+      .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
+        .tarjeta.h-100.d-flex.align-items-center.p-4
+          figure
+            img(src="@/assets/template/arrastrar.svg", alt="Texto que describa la imagen")
+      .col-12.col-md-8.col-lg-9
+        .titulo-segundo
+          h2 Actividad didactica 2 - Reforzando conceptos sobre Control de labores en la producción agrícola
+        p.mb-4 Reforzar los conceptos y habilidades en Control de labores en la producción agrícola, con base en lo estudiado en componente formativo.
+        .tarjeta.actividad.p-3
+          .row.justify-content-around.align-items-center            
+            .col-sm.mb-3.mb-sm-0
+              p.fw-bold.mb-0 Arrastrar y soltar
+            .col-auto
+              a.boton.boton--b(:href="obtenerLink('/actividades_2/story.html')" target="_blank")
+                span Realizar
+                i.fas.fa-puzzle-piece
+    
+    
    
 
 </template>
